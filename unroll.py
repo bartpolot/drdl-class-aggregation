e4a076fde4869dc8976fb3215b08940b154102c3
Bart Polot <bart@polot.org>
2020-10-14 23:31:33 +0200

    add id of parent class, if any

--- unroll.py
+++ unroll.py
@@ -2,6 +2,17 @@
 #
 # PoC code: no warranty, might explode, harmful to children, etc
 #
+# File structure of a DRDL:
+# schema:
+# - db: apt
+#   tables:
+#   - table: TABLE_NAME
+#     collection: COLLECTION_NAME
+#     pipeline:
+#     - $stage1: [...]
+#     - $stage2: [...]
+#     columns: [{ MongoType, Name, SqlName, SqlType }]
+#
 # TODO: 
 # - handle entities under recursive objects, eg: 
 #   - quote_coverageList_insuredEntityList
@@ -18,9 +29,27 @@ DST_FILENAME = "dst.drdl"
 import yaml
 import re
 
-def buildBasePipeline(name):
+def removeDuplicates(x):
+    return list(dict.fromkeys(x))
+
+def getDocumentClass(tableName):
+    return tableName.split("_")[-1].replace("List", "")
+
+def getDocumentParentClass(tableName):
+    elements = removeDuplicates(tableName.split("_"))
+    if ( len(elements) >= 2 ):
+        element = elements[-2].replace("List", "")
+    else:
+        element = ""
+    return element
+
+
+def buildBasePipeline(name, parentName=None):
     unwind = { "$unwind": { "path": "$" + name, "preserveNullAndEmptyArrays": False } }
-    addFields = { "$addFields": { name + ".pid": "$" + ID_NAME} }
+    if (parentName):
+        addFields = { "$addFields": { name + "." + parentName + "_id": "$_id" } }
+    else:
+        addFields = { "$addFields": { name + ".pid": "$" + ID_NAME} }
     replaceRoot = { "$replaceRoot": { "newRoot": "$" + name } }
     project = { "$project": { name: 0 }}
     return [unwind, addFields, replaceRoot, project]
@@ -40,8 +69,9 @@ def buildUnionStage(name, level):
 
 def replacePipeline(name, info):
     table = info["table"]
-    table["table"] = "Recursive" + name
-    newPipeline = buildBasePipeline(name)
+    parentName = getDocumentParentClass(table["table"])
+    table["table"] = getDocumentClass(name)
+    newPipeline = buildBasePipeline(name, parentName)
     for i in range(1, info["c"]):
         newPipeline.append(buildUnionStage(name, i))
     table["pipeline"] = newPipeline
@@ -58,6 +88,10 @@ def replaceColumns(name, info):
         column["SqlName"] = re.sub(colNameRegex, "", column["SqlName"])
         newColumns.append(column)
     newColumns.append({"MongoType": "bson.ObjectId", "Name": "pid", "SqlName": "pid", "SqlType": "objectid"})
+    parentName = getDocumentParentClass(table["table"])
+    if (parentName):
+        parentFieldName = parentName + "_id"
+        newColumns.append({"MongoType": "bson.ObjectId", "Name": parentFieldName, "SqlName": parentFieldName, "SqlType": "objectid"})
     table["columns"] = newColumns
 
 def isRecursiveSubClass(name, table):
@@ -68,37 +102,35 @@ def isRecursiveSubClass(name, table):
 def removeTables(name, db):
     db["tables"][:] = [ table for table in db["tables"] if not isRecursiveSubClass(name, table)]
 
-# File structure:
-# schema:
-# - db: apt
-#   tables:
-#   - table: coverages_short
-#     collection: quotes
-#     pipeline:
-#     - $stage1: [...]
-#     - $stage2: [...]
+
 with open(SRC_FILENAME, 'r') as stream:
     recursiveNameRegex = r"(^|_)([a-zA-Z0-9]+)_\2(_|$)"
     for schemas in yaml.load_all(stream):
         for schema, dbs in schemas.items():
             for db in dbs:
                 recursiveClasses = {}
+                tableIndex = {}
                 for table in db["tables"]:
                     tableName = table["table"]
+                    className = getDocumentClass(tableName)
+                    if className in tableIndex:
+                        tableIndex[className].append(tableName)
+                    else:
+                        tableIndex[className] = [tableName]
                     matches = re.findall(recursiveNameRegex, tableName)
                     if (matches):
                         recursiveClass = matches[0][1]
                         c = tableName.count(recursiveClass)
-                        try:
+                        if recursiveClass in recursiveClasses:
                             currentMax = recursiveClasses[recursiveClass]
-                        except KeyError:
+                        else:
                             currentMax = {"c": 0}
-                        if (currentMax["c"] < c):
+                        if (c > currentMax["c"]):
                             recursiveClasses[recursiveClass] = {"c": c, "tableName": tableName, "table": table}
                 for recursiveClass, info in recursiveClasses.items():
-                    print(recursiveClass, "in", info["tableName"])
-                    replacePipeline(recursiveClass, info)
+                    #print(recursiveClass, "in", info["tableName"])
                     replaceColumns(recursiveClass, info)
+                    replacePipeline(recursiveClass, info)
                     removeTables(recursiveClass, db)
         with open(DST_FILENAME, "w") as outFile:
             outFile.write(yaml.dump(schemas, default_flow_style=False))
