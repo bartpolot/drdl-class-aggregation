c25f1273b2fa78a8b42ebea2f33a7ebb92539753
Bart Polot <bart@polot.org>
2020-10-17 02:51:16 +0200

    refactor to use column names to detect embedded objects

--- aggregate.py
+++ aggregate.py
@@ -4,7 +4,7 @@
 #
 # File structure of a DRDL:
 # schema:
-# - db: apt
+# - db: DB_NAME
 #   tables:
 #   - table: TABLE_NAME
 #     collection: COLLECTION_NAME
@@ -33,29 +33,48 @@ def columnNameCleanup(className, columnName):
     colNameRegex = r".*" + className + r"(|List)\."
     return re.sub(colNameRegex, "", columnName)
 
-def getParentObjectName(tableName):
-    objectNames = tableName.split("_")
-    if (len(objectNames) < 2):
+def getParentObjectName(classPath):
+    if (len(classPath) < 2):
         return None
-    if (objectNames[-2] == objectNames[-1]):
+    if (classPath[-2] == classPath[-1]):
         return "pid"
-    return objectNames[-2].replace("List", "") + "_id"
+    return classPath[-2].replace("List", "") + "_id"
 
-def addParentColumn(columnIndex, table):
-    parentName = getParentObjectName(table["table"])
+def addParentColumn(columnIndex, classPath):
+    parentName = getParentObjectName(classPath)
     if (parentName and parentName not in columnIndex):
         columnIndex[parentName] = {"MongoType": "bson.ObjectId", "Name": parentName, "SqlName": parentName, "SqlType": "objectid"}
 
-def buildColumns(name, tables):
-    """ Aggregate the columns from all tables that contain this class except idx columns """
+def buildColumns(classInfo, idx):
+    """ Aggregate the columns from all tables that contain this class except idx columns 
+
+    classInfo: [{ 'className': className, 'table': table, 'classPath': ['path', 'to', 'class']] }] """
     columnIndex = {}
-    for table in tables:
+    className = classInfo[0]["className"]
+    print(className)
+    for srcInfo in classInfo:
+        table = srcInfo["table"]
+        print(" ", srcInfo["classPath"], " - ", srcInfo["table"]["table"])
         for column in [column for column in table["columns"] if "idx" not in column["Name"]]:
-            columnName = columnNameCleanup(name, column["Name"])
-            columnIndex[columnName] = column
-            column["Name"] = columnName
-            column["SqlName"] = columnName
-        addParentColumn(columnIndex, table)
+            columnName = columnNameCleanup(srcInfo["className"], column["Name"])
+            if columnName == "_id":
+                print("      _id is not allowed in nested objects")
+                continue
+            if className not in column["Name"]: # this a field of the parent
+                print("     ", column["Name"], "belongs to the parent class")
+                continue
+            if columnName in columnIndex: # already have it from other tables
+                print("     ", columnName ,"- we've already seen this")
+                continue
+            if columnName.split(".")[0] in idx: # this is a full-time class
+                print("     ", columnName.split(".")[0], "belongs in a separate table")
+                continue
+            print("   ", columnName, " <-- ", column["Name"])
+            newColumn = dict(column)
+            newColumn["Name"] = columnName
+            newColumn["SqlName"] = columnName
+            columnIndex[columnName] = newColumn
+        addParentColumn(columnIndex, srcInfo["classPath"])
     return list(columnIndex.values())
 
 ################ Pipeline ################
@@ -89,23 +108,24 @@ def buildUnionStage(name, stages, collectionName):
     stage = { "$unionWith": { "coll": collectionName, "pipeline": internalPipeline } }
     return stage
 
-def buildPipeline(name, tables):
+def buildPipeline(classInfo):
     """ Builds a pipeline to collect instances of a class from all possible nesting levels """
-    initialStages = tables[0]["table"].split("_")
+    collectionName = classInfo[0]["table"]["collection"]
+    initialStages = classInfo[0]["classPath"]
+    className = classInfo[0]["className"]
     if (len(initialStages) < 2):
         return []
-    pipeline = buildGenericPipeline (name, initialStages)
-    collectionName = tables[0]["collection"]
-    for table in tables[1:]:
-        stages = table["table"].split("_")
+    pipeline = buildGenericPipeline (className, initialStages)
+    for srcInfo in classInfo[1:]:
+        stages = srcInfo["classPath"]
         if(len(stages) < 2):
             continue
-        pipeline.append(buildUnionStage(name, stages, collectionName))
+        pipeline.append(buildUnionStage(className, stages, collectionName))
     return pipeline
 
 ################ Table ################
 
-def buildClassTable(name, tables):
+def buildClassTable(classInfo, idx):
     """ Builds a Table definition for a class
 
     The table definition will contain:
@@ -113,28 +133,57 @@ def buildClassTable(name, tables):
     - The collection where the class is stored
     - The columns of this table
     - The aggregation pipeline needed to generate this table """
-    dst = {"table": name, "collection": tables[0]["collection"]}
-    dst["columns"] = buildColumns(name, tables)
-    dst["pipeline"] = buildPipeline(name, tables)
+    dst = {"table": classInfo[0]["className"],
+          "collection": classInfo[0]["table"]["collection"]}
+    dst["columns"] = buildColumns(classInfo, idx)
+    dst["pipeline"] = buildPipeline(classInfo)
     return dst
 
- ################ Classes ################
+################ Classes ################
 
-def getDocumentClassName(tableName):
+def printClassIndex(classIndex):
+    """ debug function """
+    print("Nested classes detected:")
+    for name, infos in classIndex.items():
+        print(" ",name)
+        for i in infos:
+            print("   ", i["classPath"])
+
+def getDocumentClassNameFromTable(tableName):
     return tableName.split("_")[-1].replace("List", "")
 
+def getDocumentClassNameFromColumn(columnName):
+    return columnName.split(".")[-2].replace("List", "")
+
+def buildClassPath(column, table):
+    """ Given a table name (for the name of the root) and
+        the column name of the oid field, 
+        build the nesting path to the class """
+    classPath = [table["table"].split("_")[0]]
+    classPath.extend(column["Name"].split(".")[:-1])
+    return classPath
+
 def buildClassIndex(db):
-    """ Build a dictionary of all classes in the database
+    """ Build a dictionary of all nested classes in the database
 
     The dictionary contains each class as key and as value
-    an array of all tables that contain this class """
+    an array of all tables that contain this class with info about
+    the nesting depth for the class.
+
+    { 'className': className, 'table': table, 'classPath': ['path', 'to', 'class']] }
+     """
     classIndex = {}
     for table in db["tables"]:
-        className = getDocumentClassName(table["table"])
-        if className in classIndex:
-            classIndex[className].append(table)
-        else:
-            classIndex[className] = [table]
+        for column in [column for column in table["columns"] if ID_NAME in column["Name"]]:
+            className = getDocumentClassNameFromColumn(column["Name"])
+            srcInfo = { 'className': className, 
+                        'table': table,
+                        'classPath': buildClassPath(column, table) }
+            if className in classIndex:
+                classIndex[className].append(srcInfo)
+            else:
+                classIndex[className] = [srcInfo]
+    printClassIndex(classIndex)
     return classIndex
 
 ################ Main ################
@@ -144,9 +193,10 @@ with open(SRC_FILENAME, 'r') as stream:
         for schema, dbs in schemas.items():
             for db in dbs:
                 classIndex = buildClassIndex(db)
-                unrolledTables = []
-                for className in classIndex:
-                    unrolledTables.append(buildClassTable(className, classIndex[className]))
+                # the root table is not nested, must be added explicitly
+                unrolledTables = [db["tables"][0]]
+                for classInfo in classIndex.values():
+                    unrolledTables.append(buildClassTable(classInfo, classIndex))
                 db["tables"] = unrolledTables
         with open(DST_FILENAME, "w") as outFile:
             outFile.write(yaml.dump(schemas, default_flow_style=False))
